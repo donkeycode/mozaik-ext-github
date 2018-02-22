2d5484c90c1e517099f37bc23dfff1016bdc1b34
Kevin Bunel <kevinbunel@gmail.com>
2018-02-22 17:22:52 +0100

    up

--- es/components/last-pr-multi-repos/LastPrMultiRepos.js
+++ es/components/last-pr-multi-repos/LastPrMultiRepos.js
@@ -37,7 +37,12 @@ var LastPrMultiRepos = function (_Component) {
 
         var body = React.createElement(WidgetLoader, null);
         var count = 0;
+        if (apiError) {
+            console.log('Error');
+            consoole.log(apiError);
+        }
         if (apiData) {
+            console.log('data');
             console.log(apiData);
             // body = (
             //     <div>
